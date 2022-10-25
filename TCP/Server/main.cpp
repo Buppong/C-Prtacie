4a355b4ef56c836cfde8ae690325c9e818e0974d
Buppong <changbok2453@naver.com>
2022-10-25 11:29:35 +0900

    소켓기본구조만드는중

--- TCP/Server/main.cpp
+++ TCP/Server/main.cpp
@@ -5,19 +5,14 @@
 
 using namespace std;
 
-/*소켓프로그래밍 함수와 Winsock2 소켓프로그래밍을 위한 함수에 대해서 이해할수있습니ㅣ다.
-
-socket()
-
-
-*/
 
 void ShowErrorMessage(string message) {
 
-	cout << "{오류발생}:" << message << '\n';
+	cout << "{오류발생}:" << message << '\n'; //오류메시지를 출력하고
 	system("pause");
-	exit(1);
-}
+	exit(1); //프로그램을 종료함
+} 
+//네트워크 프로그래밍은 오류상황이 많이날수있는데 그래서 오류관련 핸들링함수는 필수이다.
 
 
 int main() {
@@ -32,6 +27,8 @@ char received[256];
 
 if (WSAStartup) {
 
-	test
+	ShowErrorMessage("WSAStartup()");
+
+	serverSocket = socket(PF_IMET, SOCK_STREAM, 0);
 }
 
