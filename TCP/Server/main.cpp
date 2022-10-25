a72ec0747f1ab759796cca6f968ad940064940e5
Buppong <changbok2453@naver.com>
2022-10-25 10:50:55 +0900

    test

--- TCP/Server/main.cpp
+++ TCP/Server/main.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+/*소켓프로그래밍 함수와 Winsock2 소켓프로그래밍을 위한 함수에 대해서 이해할수있습니ㅣ다.
+
+socket()
+
+
+*/
+
 void ShowErrorMessage(string message) {
 
 	cout << "{오류발생}:" << message << '\n';
@@ -23,4 +30,8 @@ int main() {
 int SeverPort = 9876;
 char received[256];
 
-if(WSAStartup)
+if (WSAStartup) {
+
+	test
+}
+
